5727fa9301dbba699afae97946fd20fbd9c89629
DAIGO <tigacosmos72@gmail.com>
2020-11-21 10:33:41 +0800

    update version

--- library/src/main/java/com/self/lib/net/ListDefaultEmptyAdapter.kt
+++ library/src/main/java/com/self/lib/net/ListDefaultEmptyAdapter.kt
@@ -3,20 +3,27 @@ package com.self.lib.net
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
-import com.self.lib.util.GsonUtil
+import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 import java.util.Collections
 
 /**
- *
- *
  * @date 2019-06-13
  */
 
 class ListDefaultEmptyAdapter : JsonDeserializer<List<*>> {
     override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): List<*> {
         return if(json != null && json.isJsonArray){
-            GsonUtil.parseJsonElement(json, typeOfT)
+            val array = json.asJsonArray
+            val itemType: Type = (typeOfT as ParameterizedType).actualTypeArguments[0]
+            val list = mutableListOf<Any>()
+            for (i in 0 until array.size()) {
+                val element = array[i]
+                context?.deserialize<Any>(element, itemType)?.let { item ->
+                    list.add(item)
+                }
+            }
+            list
         }else{
             Collections.EMPTY_LIST
         }
---
+++ library/src/main/java/com/self/lib/net/NumberDefaultAdapter.kt
@@ -0,0 +1,32 @@
+package com.self.lib.net
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import java.lang.RuntimeException
+import java.lang.reflect.Type
+
+/**
+ *
+ * @author MBP
+ * @date 2020/11/21
+ */
+class NumberDefaultAdapter : JsonSerializer<Number>, JsonDeserializer<Number> {
+    override fun serialize(src: Number?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+        return JsonPrimitive(src)
+    }
+
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Number {
+        try {
+            if(json != null && !json.isJsonNull){
+                return json.asNumber
+            }
+        }catch (e: RuntimeException){
+            e.printStackTrace()
+        }
+        return -1
+    }
+}
